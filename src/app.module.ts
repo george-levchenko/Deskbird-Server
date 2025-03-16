593e282a38658cf4416a0a8239b5828a974c40f8
George <levchenko.george.vata@gmail.com>
2025-03-16 20:54:35 +0100

    pg package, run fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
+import { User } from './modules/users/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'nestjs',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
